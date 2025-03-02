e38d1b2a7fbca8ed95fe8c3639c3ac793b2b51e4
GuiM <Guilherme.Marrafon1@gmail.com>
2025-03-02 20:43:19 -0300

    update

--- aula012ex/ex014/script.js
+++ aula012ex/ex014/script.js
@@ -1,1 +1,20 @@
-alert('ola')
+function carregar() {
+let msg = window.document.getElementById('msg')
+let img = window.document.getElementById('imagem')
+let data = new Date()
+let hora = data.getHours()
+msg.innerHTML = `Agora são ${hora} horas.`
+if (hora >= 0 && hora <= 12) {
+    //manhã
+    img.src = 'imagens/manha.png'
+    document.body.style.background = '#FFCC93'
+} else if ( hora >= 12 && hora <= 18) {
+    //tarde
+    img.src = 'imagens/tarde.png'
+    document.body.style.background = '#913802'
+} else {
+    img.src = 'imagens/noite.png'
+    document.body.style.background = '#005AA5'
+    //noite
+}
+}
