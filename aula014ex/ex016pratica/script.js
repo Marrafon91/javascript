2810da48ecbe4235786f130362a29436df92fdee
GuiM <Guilherme.Marrafon1@gmail.com>
2025-05-28 13:13:28 -0300

    Summary

--- aula014ex/ex016pratica/script.js
+++ aula014ex/ex016pratica/script.js
@@ -1,30 +1,25 @@
 function contar() {
-    let ini = document.getElementById('txti')
-    let fim = document.getElementById('txtf')
-    let passo = document.getElementById('txtp')
-    let res = document.getElementById('res')
+  let ini = Number(document.getElementById("txti").value);
+  let fim = Number(document.getElementById("txtf").value);
+  let passo = Number(document.getElementById("txtp").value);
+  let res = document.getElementById("res");
 
-    if (ini.value == 0 || fim.value == 0 || passo.value == 0) {
-        window.alert('[ERRO] Faltam dados!')
-        res.innerHTML = 'Impossivel contar'
-        
+  if (ini == 0 || fim == 0 || passo == 0) {
+    window.alert("[ERRO] Faltam dados!");
+    res.innerHTML = "Impossivel contar";
+  } else {
+    res.innerHTML = "Contando: <br>";
+    if (ini <= fim) {
+      //contagem crescente
+      for (let c = ini; c <= fim; c += passo) {
+        res.innerHTML += `${c} ↠`;
+      }
     } else {
-        res.innerHTML = 'Contando: <br>'
-        let i = Number(ini.value)
-        let f = Number(fim.value)
-        let p = Number(passo.value)
-
-        if (i <= f) {
-            //contagem crescente
-            for(let c = i; c <= f; c += p) {
-                res.innerHTML += `${c} ↠`
-            }
-        } else {
-            //contagem regressiva
-            for(let c = i; c >= f; c -= p) {
-                res.innerHTML += `${c} ↞`
-            }
-        }
-        res.innerHTML += '♛Muito Bem♛'
+      //contagem regressiva
+      for (let c = ini; c >= fim; c -= passo) {
+        res.innerHTML += `${c} ↞`;
+      }
     }
+    res.innerHTML += "♛Muito Bem♛";
+  }
 }
